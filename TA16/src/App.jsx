022fe20e52592344f1e245201b325b0a08361c03
Eze <eze.gonzalez@vera.com.uy>
2024-10-07 08:43:06 -0300

    faltaban los links

--- TA16/src/App.jsx
+++ TA16/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
@@ -13,6 +13,10 @@ function App() {
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
             </Routes>
+
+            <Link to="/home">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
         </>
     )
 }
